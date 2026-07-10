c3f5d4312768a4403dbcb418a4709678acf6d99e
dev <devnarayanpunthala3@gmail.com>
2026-07-10 13:51:09 +0530

    Update ex19.py

--- python programs/ex19.py
+++ python programs/ex19.py
@@ -6,3 +6,25 @@ for i in list1:
       if(i%2==0): 
            list1.remove(i) 
 print(list1) 
+
+
+
+def total_jumps(heights, X, Y):
+    total = 0
+
+    for h in heights:
+        jumps = 1
+        reach = X
+
+        while reach < h:
+            reach += (X - Y)
+            jumps += 1
+
+        total += jumps
+
+    return total
+
+
+# Example
+print(total_jumps([11, 11], 10, 1))        # 4
+print(total_jumps([11, 10, 10, 9], 10, 1)) # 5
